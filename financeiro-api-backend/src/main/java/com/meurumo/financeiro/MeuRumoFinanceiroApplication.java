43e5cf8f6ba2ab212744a49a220dcb731e1d5305
Fábio André Zatta <fisiofaz8@gmail.com>
2026-03-06 06:16:26 -0300

    chore: inicialização do projeto Meu Rumo Financeiro com Spring Boot

---
+++ financeiro-api-backend/src/main/java/com/meurumo/financeiro/MeuRumoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.meurumo.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuRumoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuRumoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ financeiro-api-backend/src/test/java/com/meurumo/financeiro/MeuRumoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meurumo.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuRumoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
