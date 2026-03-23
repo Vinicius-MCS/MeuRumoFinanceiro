b66bfe765e22be2cdd35c582073b50d8920eb330
Bárbara Gonçalves <159402409+BarbaraGoncalves28@users.noreply.github.com>
2026-03-23 13:05:24 -0300

    Investir, Cartões, Drop Down

--- financeiro-ui-frontend/src/App.tsx
+++ financeiro-ui-frontend/src/App.tsx
@@ -1,16 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashboardCard from "./components/DashboardCard";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Invest from "./components/Invest";
+import Cards from "./components/Cards";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-blue-600">
-          Tailwind Configurado com Sucesso! 🚀
-        </h1>
-        <p className="mt-2 text-gray-600">
-          Se você está vendo este texto estilizado, o financeiro-ui-frontend
-          está pronto.
-        </p>
-      </div>
+    <BrowserRouter>
+        <div className="min-h-screen bg-gray-100 flex flex-col">
+        <Navbar />
+
+        <main className="flex-1 pb-24">
+          <Routes>
+            <Route path="/" element={<DashboardCard />}/>
+            <Route path="/invest" element={<Invest/>}/>
+            <Route path="/cards" element={<Cards/>}/>
+            <Route/>
+          </Routes>
+        </main>
+
+      <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
